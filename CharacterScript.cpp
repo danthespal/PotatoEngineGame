3b385cd3bb1b3d2cace3a6736dded3909a064c77
danthespal <danielplanteskole@gmail.com>
2024-07-06 01:47:50 +0300

    rework for register a script

---
+++ CharacterScript.cpp
@@ -0,0 +1,10 @@
+#include "CharacterScript.h"
+
+namespace potato_engine_game {
+
+void character_script::update(float dt)
+{
+	// do character update
+}
+
+}
--- CharacterScript.h
+++ CharacterScript.h
@@ -2,8 +2,15 @@
 
 namespace potato_engine_game {
 
+REGISTER_SCRIPT(character_script);
+
 class character_script : public PotatoEngine::script::entity_script
 {
+public:
+	constexpr explicit character_script(PotatoEngine::game_entity::entity entity)
+		: PotatoEngine::script::entity_script(entity) {}
 
+	void update(float dt) override;
 };
+
 }
